e8bf932c45858d19377a478130f9618d049f7e64
Yogeshkumarthakur <yogeshkumarthakur@gmail.com>
2020-05-03 13:18:45 +0530

    added search page and login page code

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -2,4 +2,11 @@ package com.pages;
 
 public class LoginPage {
 
+	public void loginPage() {
+		System.out.println("loginPage");
+	}
+	
+	public void loginPagesetup() {
+		System.out.println("loginPagesetup");
+	}
 }
