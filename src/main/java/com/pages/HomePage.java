e7a932e0243eb9b9f28832eb90b37590b3945a4c
Yogeshkumarthakur <yogeshkumarthakur@gmail.com>
2020-05-04 09:42:50 +0530

    review comment incorporated

--- src/main/java/com/pages/HomePage.java
+++ src/main/java/com/pages/HomePage.java
@@ -5,6 +5,6 @@ public class HomePage {
 
 	public void homepage() {
 		System.out.println("Home Page testing with freature branch");
-	
+		System.out.println("Home Page review commnet incorporated");
 	}
 }
